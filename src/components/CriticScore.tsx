91f4a1cff1a86744defaa9c86ed5474c4e0b462c
Nikos Michalakis <nikos@drtechniko.com>
2024-10-17 22:03:58 +0200

    Reformat code

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -1,16 +1,23 @@
-import { Badge } from '@chakra-ui/react';
-import React from 'react';
+import { Badge } from "@chakra-ui/react";
+import React from "react";
 
 interface CriticScoreProps {
-    score: number;
+  score: number;
 }
 
 const CriticScore: React.FC<CriticScoreProps> = ({ score }) => {
-    const color = score > 75 ? 'green' : score > 50 ? 'yellow' : '';
+  const color = score > 75 ? "green" : score > 50 ? "yellow" : "";
 
-    return (
-        <Badge colorScheme={color} fontSize={'14px'} paddingX={2} borderRadius={'4px'}>{score}</Badge>
-    );
+  return (
+    <Badge
+      colorScheme={color}
+      fontSize={"14px"}
+      paddingX={2}
+      borderRadius={"4px"}
+    >
+      {score}
+    </Badge>
+  );
 };
 
 export default CriticScore;
