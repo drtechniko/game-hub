07e67b8dcbe2ca0c1aa36f50428b0252f95b2c03
Nikos Michalakis <nikos@drtechniko.com>
2024-10-17 22:03:27 +0200

    Display critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,16 @@
+import { Badge } from '@chakra-ui/react';
+import React from 'react';
+
+interface CriticScoreProps {
+    score: number;
+}
+
+const CriticScore: React.FC<CriticScoreProps> = ({ score }) => {
+    const color = score > 75 ? 'green' : score > 50 ? 'yellow' : '';
+
+    return (
+        <Badge colorScheme={color} fontSize={'14px'} paddingX={2} borderRadius={'4px'}>{score}</Badge>
+    );
+};
+
+export default CriticScore;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,43 +1,45 @@
-import React from 'react';
-import apiClient from '../services/api-client';
-import { CanceledError } from 'axios';
+import React from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
 
 export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
-}   
+  id: number;
+  name: string;
+  slug: string;
+}
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: { platform: Platform }[];
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
 interface FetchGameResponse {
-    count: number;
-    results: Game[];
+  count: number;
+  results: Game[];
 }
 
 const useGames = () => {
-    const [games, setGames] = React.useState<Game[]>([]);
-    const [error, setError] = React.useState('');
+  const [games, setGames] = React.useState<Game[]>([]);
+  const [error, setError] = React.useState("");
 
-    React.useEffect(() => {
-        const controller = new AbortController();
-        
-        apiClient.get<FetchGameResponse>('/games', { signal: controller.signal })
-            .then(res => setGames(res.data.results))
-            .catch(err => {
-                if (err instanceof CanceledError) return;             
-                setError(err.message);
-            });
+  React.useEffect(() => {
+    const controller = new AbortController();
 
-        return () => controller.abort();
-    }, []);
+    apiClient
+      .get<FetchGameResponse>("/games", { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
 
-    return { games, error };
-}
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
 
 export default useGames;
