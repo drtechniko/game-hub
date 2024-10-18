33b676e31389c9a2b2be6f7d9dedc2f94e1ba3c6
Nikos Michalakis <nikos@drtechniko.com>
2024-10-18 22:26:30 +0200

    Create generic data hook

---
+++ src/hooks/useData.ts
@@ -0,0 +1,37 @@
+import { CanceledError } from "axios";
+import React from "react";
+import apiClient from "../services/api-client";
+
+interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
+const useData = <T>(endpoint: string) => {
+  const [data, setData] = React.useState<T[]>([]);
+  const [error, setError] = React.useState("");
+  const [isLoading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
+    apiClient
+      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
+      .then((res) => {
+        setData(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, [endpoint]);
+
+  return { data, error, isLoading };
+};
+
+export default useData;
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,42 +1,10 @@
-import { CanceledError } from "axios";
-import React from "react";
-import apiClient from "../services/api-client";
+import useData from "./useData";
 
-interface Genre {
+export interface Genre {
   id: number;
   name: string;
 }
 
-interface FetchGenreResponse {
-  count: number;
-  results: Genre[];
-}
-
-const useGenres = () => {
-    const [genres, setGenres] = React.useState<Genre[]>([]);
-    const [error, setError] = React.useState("");
-    const [isLoading, setLoading] = React.useState(false);
-
-    React.useEffect(() => {
-      const controller = new AbortController();
-
-      setLoading(true);
-      apiClient
-        .get<FetchGenreResponse>("/genres", { signal: controller.signal })
-        .then((res) => {
-          setGenres(res.data.results);
-          setLoading(false);
-        })
-        .catch((err) => {
-          if (err instanceof CanceledError) return;
-          setError(err.message);
-          setLoading(false);
-        });
-
-      return () => controller.abort();
-    }, []);
-
-    return { genres, error, isLoading };
-};
+const useGenres = () => useData<Genre>("/genres");
 
 export default useGenres;
